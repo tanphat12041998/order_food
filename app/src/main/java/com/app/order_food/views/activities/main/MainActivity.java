25378de2a890ae4c549f9cf3ac8f41d31da20905
HoXuanQuang23 <quanghxps08737@fpt.edu.vn>
2021-01-04 14:39:02 +0700

    update model update implementation API update api - retrofitclient update navigation

--- app/src/main/java/com/app/order_food/views/activities/main/MainActivity.java
+++ app/src/main/java/com/app/order_food/views/activities/main/MainActivity.java
@@ -1,23 +1,53 @@
 package com.app.order_food.views.activities.main;
 
+import android.annotation.SuppressLint;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.app.order_food.R;
 import com.app.order_food.views.activities.BaseActivity;
+import com.app.order_food.views.fragments.AccountFragment;
+import com.app.order_food.views.fragments.CartFragment;
+import com.app.order_food.views.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends BaseActivity {
 
 
     @Override
     protected void initialViewComponent() {
-        // TODO
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+    @SuppressLint("NonConstantResourceId")
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            menuItem -> {
+                Fragment selectedFragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_cart:
+                        selectedFragment = new CartFragment();
+                        break;
+                    case R.id.nav_user:
+                        selectedFragment = new AccountFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            };
 
     @Override
     protected void initialVariables() {
-        // TODO
+
     }
 
     @Override
     protected int getLayoutId() {
-        return R.layout.activity_main;
+        return R.layout.activity_home;
     }
 }
