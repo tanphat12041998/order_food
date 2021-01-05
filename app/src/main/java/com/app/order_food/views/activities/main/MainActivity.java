6f115ad570cd7b7b1913ac4ca2cd1251fe54e867
thanhlam12061998 <thanhlam12061998@gmail.com>
2021-01-05 10:18:28 +0700

    layout setting

--- app/src/main/java/com/app/order_food/views/activities/main/MainActivity.java
+++ app/src/main/java/com/app/order_food/views/activities/main/MainActivity.java
@@ -1,6 +1,11 @@
 package com.app.order_food.views.activities.main;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
 import com.app.order_food.R;
+import com.app.order_food.components.recyclerview.decorator.CustomDecorator;
 import com.app.order_food.views.activities.BaseActivity;
 
 public class MainActivity extends BaseActivity {
@@ -20,4 +25,11 @@ public class MainActivity extends BaseActivity {
     protected int getLayoutId() {
         return R.layout.activity_main;
     }
+
+
+    Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.logo2);
+    Bitmap circularBitmap = CustomDecorator.getRoundedCornerBitmap(bitmap, 100);
+
+    ImageView circularImageView = (ImageView) findViewById(R.id.imgAvatarUser);
+        circularImageView.();
 }
