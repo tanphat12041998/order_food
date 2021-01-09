03378bfeafdc4f123929592990d1e46ee536227f
thanhlam12061998 <thanhlam12061998@gmail.com>
2021-01-09 11:58:05 +0700

    fix layout

--- app/src/main/java/com/app/order_food/API/RetrofitClient.java
+++ app/src/main/java/com/app/order_food/API/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-        public static final String ROOT_URL = "http://192.168.0.105:3000/";
+        public static final String ROOT_URL = "http://192.168.1.32:3000/";
         public static Retrofit retrofit = null;
         public static Retrofit getClient() {
             if (retrofit == null) {
