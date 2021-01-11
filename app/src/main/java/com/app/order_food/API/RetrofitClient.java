de53f71987f70c3d52e4524b5c5de94d1a8ff66a
thanhlam12061998 <thanhlam12061998@gmail.com>
2021-01-11 10:54:26 +0700

    fragment_don_hang

--- app/src/main/java/com/app/order_food/API/RetrofitClient.java
+++ app/src/main/java/com/app/order_food/API/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-        public static final String ROOT_URL = "http://192.168.1.83:3000/";
+        public static final String ROOT_URL = "http://192.168.1.13:3000/";
 
         public static Retrofit retrofit = null;
         public static Retrofit getClient() {
